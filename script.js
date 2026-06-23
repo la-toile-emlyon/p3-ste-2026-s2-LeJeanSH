8c0900f88ebbbf0bced7f2ccb1729d4ab24e7e85
Ludivine Bocquier <98520169+ludivineboc@users.noreply.github.com>
2026-06-23 12:57:36 +0200

    mardi AM

--- script.js
+++ script.js
@@ -20,6 +20,22 @@ var swiper = new Swiper(".mySwiper", {
     });
 
 
+let lastScrollY = window.scrollY;
+const header = document.querySelector('.main-header');
+
+window.addEventListener('scroll', () => {
+  const currentY = window.scrollY;
+
+  if (currentY > 40 && currentY > lastScrollY) {
+    // on descend → on cache le header
+    header.classList.add('hidden');
+  } else {
+    // on remonte → on le réaffiche
+    header.classList.remove('hidden');
+  }
+
+  lastScrollY = currentY;
+}, { passive: true });
 
 
 
